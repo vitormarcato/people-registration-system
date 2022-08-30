f56b71afbe6bf88a4dc08e76830fd2c89d379145
Vitor Marcato <vitor.marcatof@gmail.com>
2022-08-30 14:24:08 -0300

    refactor: update  App class with switch case

--- src/App.java
+++ src/App.java
@@ -19,31 +19,26 @@ public class App {
 		printWelcome.welcomeSystemScreen();
 		int userOption = Integer.parseInt(sc.nextLine());
 		
-		if (userOption == 1) {
-			RegisterPersonView createRegister = new RegisterPersonView();
-			createRegister.printRegisterView();
-			
-		} else if (userOption == 2) {
-			SearchView searchPerson = new SearchView(); 
-			searchPerson.printSearchView();
-			
-		} else if (userOption == 3) {
-			UpdateInformationView updateInformation = new UpdateInformationView();
-			updateInformation.printUpdateInformation();			
-
-		} else if (userOption == 4) {
-			DeleteView deleteInformation = new DeleteView();
-			deleteInformation.printDelete();
-		
-		} else if (userOption == 5) {
-			ShowListView showList = new ShowListView();
-			showList.printList();	
-			
-		} else if (userOption == 0) {
-			System.exit(0);
-		} else {
-			System.out.println("Please, try again.");
+		switch (userOption) {	
+		case 1: RegisterPersonView createRegister = new RegisterPersonView();
+		createRegister.printRegisterView();
+		break;
+		case 2: SearchView searchPerson = new SearchView(); 
+		searchPerson.printSearchView();
+		break;
+		case 3:	UpdateInformationView updateInformation = new UpdateInformationView();
+		updateInformation.printUpdateInformation();	
+		break;
+		case 4:	DeleteView deleteInformation = new DeleteView();
+		deleteInformation.printDelete();
+		break;
+		case 5:	ShowListView showList = new ShowListView();
+		showList.printList();
+		break;
+		case 0: System.exit(0);
+		default: System.out.println("Please, try again.");
 		}
+
 		sc.close();
 	}
 		
